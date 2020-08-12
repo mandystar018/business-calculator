d4ad55e832bb0f800b3f13f4eadab2e291a4c415
David Cole <davidtcole@yahoo.com>
2020-08-12 13:19:27 -0700

    complete the JQuery for the business calculator by adding new methods and variables in order to correctly output to the user.
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,11 @@ const divide = function(number1, number2) {
 //UI Logic
 
 $(document).ready(function(){
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
 });
