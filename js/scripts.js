91b6008dbf8b5d5b55b5afbd9af4992ecbe25684
David Cole <davidtcole@yahoo.com>
2020-08-12 11:44:27 -0700

    add new files and folders to our calculator project and included new separation of business logic and ui logic. Also added a form for the user to input numbers.
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
-function add(number1, number2) {
-	return number1 + number2;
-}
+//Business Logic
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+const add = function(number1, number2) {
+  return number1 + number2;
+};
 
-alert(result);
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
 
+//UI Logic
 
-function temperatureConvert() {
-  const farenheit = parseInt(prompt("Enter a temperature in Farenheit: "));
-  return convertTemp(farenheit);
-} 
-
-function convertTemp (farenheit) {
-  const result = (farenheit -32 ) * (5/9);
-  return result;
-}
-
-const outputMessage = temperatureConvert();
-alert(outputMessage);
+$(document).ready(function(){
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  
+  alert(add(number1, number2));
+});
